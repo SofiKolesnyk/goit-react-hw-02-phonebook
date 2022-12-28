9ba7fcbe60ac6ef6095a6ec14e8ab7fd3a2ba5cb
SofiKolesnyk <sofi_serg@ukr.net>
2022-12-28 22:17:42 +0200

    hw2-2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,49 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import { Notify } from 'notiflix';
+import Section from './Section';
+import ContactForm from './ContactForm';
+import Contacts from './Contacts';
+
+export default class App extends Component {
+  state = {
+    contacts: [],
+  }; 
+
+  onGetDataForm = (data) => {
+    const hasName = this.state.contacts.some(it => it.name === data.name);
+    if (hasName) {
+      Notify.warning(`Contact "${data.name}" is already exist.`);
+      return;
+    }
+
+    this.setState(p => ({
+      contacts: [...p.contacts, { ...data, id: nanoid() }]
+    }))
+  }
+
+  deleteItem = (deletedId) => {
+    this.setState(p => ({
+      contacts: p.contacts.filter(({id}) => id !== deletedId)
+    }))
+  }
+
+  render() {
+    const {contacts} = this.state;
+    return (
+      <div>
+        <Section title='Phonebook'>
+          <ContactForm
+            onSubmit={this.onGetDataForm}
+          />
+        </Section>
+        <Section title='Contacts'>
+          <Contacts
+            contacts={contacts}
+            onClickDelete={this.deleteItem}
+          />
+        </Section>
+      </div>
+    );
+  }
+}
